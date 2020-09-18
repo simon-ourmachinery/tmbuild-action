9ca28de44d97f9f634ef52240584e7bd58d64009
simon-ourmachinery <simon@ourmachinery.com>
2020-09-18 19:15:37 +0200

    added extra logging

--- internal/tools.js
+++ internal/tools.js
@@ -9,7 +9,7 @@ const glob = require('@actions/glob');
 const os = require('os');
 const fs = require('fs');
 
-async function chmod(file){
+async function chmod(file) {
     await e.exec(`chmod +x ${file}`);
 }
 
@@ -21,21 +21,21 @@ exports.chmod = chmod;
  * @param tool name of the tool
  * @param alternativeRepo?: alternativeRepo
  */
-async function install(tool,alternativeRepo) {
-        core.startGroup(`[tmbuild-action] install ${tool}`);
-            utils.debug(`tool: ${tool}`);
-            const repo = (alternativeRepo == undefined) ? utils.getInput("repo") : alternativeRepo;
-            const libjson = utils.parseLibsFile(utils.getInput("libjsonpath"));
-            const toolObject = (tool!="tmbuild")?utils.getLib(libjson,tool):{lib:"tmbuild"};
-            const toolname = toolObject.lib;
-            const toolUrl = `${repo}${toolname}.zip`;
-            utils.debug(`repo: ${repo}`);
-            utils.debug(`toolUrl: ${toolUrl}`);
-            utils.info(`could not find ${tool} in tools cache downloads it`);
-            const zipPath = await tc.downloadTool(`${toolUrl}`);
-            let extractedFolder = await tc.extractZip(zipPath,'./lib');
-            utils.info(`extractedFolder ${tool} path: ${extractedFolder}`);
-        core.endGroup();
+async function install(tool, alternativeRepo) {
+    core.startGroup(`[tmbuild-action] install ${tool}`);
+    utils.debug(`tool: ${tool}`);
+    const repo = (alternativeRepo == undefined) ? utils.getInput("repo") : alternativeRepo;
+    const libjson = utils.parseLibsFile(utils.getInput("libjsonpath"));
+    const toolObject = (tool != "tmbuild") ? utils.getLib(libjson, tool) : { lib: "tmbuild" };
+    const toolname = toolObject.lib;
+    const toolUrl = `${repo}${toolname}.zip`;
+    utils.debug(`repo: ${repo}`);
+    utils.debug(`toolUrl: ${toolUrl}`);
+    utils.info(`could not find ${tool} in tools cache downloads it from ${toolUrl}`);
+    const zipPath = await tc.downloadTool(`${toolUrl}`);
+    let extractedFolder = await tc.extractZip(zipPath, './lib');
+    utils.info(`extractedFolder ${tool} path: ${extractedFolder}`);
+    core.endGroup();
     return extractedFolder;
 }
 exports.install = install;
@@ -45,7 +45,7 @@ exports.install = install;
  * @param tool name of the tool or path to tool
  * @param args tool arguments (str)
  */
-async function exec(tool,args){
+async function exec(tool, args) {
     core.startGroup(`[tmbuild-action] execute ${tool}`);
     const ending = (os.platform() == "win32") ? ".exe" : "";
 
@@ -62,9 +62,9 @@ async function exec(tool,args){
     };
     options.silent = !core.isDebug();
 
-    try{
+    try {
         const libJson = utils.parseLibsFile(utils.getInput("libjsonpath"));
-        const toolPath = utils.getLibPath(libJson,tool);
+        const toolPath = utils.getLibPath(libJson, tool);
         utils.info(`${tool} is in ${toolPath}`);
         const toolCall = `${toolPath}/${tool}${ending}`;
         if (!fs.existsSync(toolCall)) throw new Error(`Error: Could not find ${tool} here: ${toolCall}`);
@@ -72,7 +72,7 @@ async function exec(tool,args){
         await e.exec(`${toolCall} ${args}`, [], options);
         utils.parseForError(myOutput);
         utils.info(`$[${toolCall} ${args}]>>\n${myOutput}\n`);
-    }catch(e){
+    } catch (e) {
         utils.parseForError(myOutput);
         utils.parseForError(myError);
         utils.info(`$[${tool} ${args}]>>\n${myOutput}\n`);
@@ -83,12 +83,12 @@ async function exec(tool,args){
 exports.exec = exec;
 
 
-async function storeFolder(artifactName,path){
+async function storeFolder(artifactName, path) {
 
     let files = [];
     const globber = await glob.create(path)
     for await (const file of globber.globGenerator()) {
-        if(!file.includes(".zip")){
+        if (!file.includes(".zip")) {
             files.push(file);
         }
     }
@@ -107,7 +107,7 @@ async function storeFile(artifactName, path) {
     let files = [];
     const globber = await glob.create(path)
     for await (const file of globber.globGenerator()) {
-            files.push(file);
+        files.push(file);
     }
     const artifactClient = artifact.create()
     const rootDirectory = '.' // Also possible to use __dirname
