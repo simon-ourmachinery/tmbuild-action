e39666fcd30dec4ff2b889f510d8822bdd6e36e3
Simon Renger <simon@ourmachinery.com>
2021-07-08 12:41:31 +0200

    added try catch which was missing

--- internal/cache.js
+++ internal/cache.js
@@ -8,8 +8,8 @@ async function set(toolPath, toolname, cacheVersion) {
     const toolsCachePath = [
         `${toolPath}`
     ]
-    utils.info(`tries to save ${toolname} to cache with path ${toolPath} with key: ${key}`);
     try {
+        utils.info(`tries to save ${toolname} to cache with path ${toolPath} with key: ${key}`);
         return await cache.saveCache(toolsCachePath, key);
     } catch (e) {
         utils.info(`tried to save ${toolname} to cache with path ${toolPath} with key: ${key} failed, proberly exists already`);
@@ -20,10 +20,14 @@ exports.set = set;
 
 async function get(toolPath, toolname, cacheVersion) {
     const key = `${toolname}-${os.platform()}-${core.getInput("config")}-${core.getInput("mode")}-${cacheVersion}`;
-    utils.info(`tries to get ${toolname} from cache with path ${toolPath} with key: ${key}`);
     const toolsCachePath = [
         `${toolPath}`
     ]
+    try {
+        utils.info(`tries to get ${toolname} from cache with path ${toolPath} with key: ${key}`);
+    } catch (e) {
+        core.warning(e.message);
+    }
     if (await cache.restoreCache(toolsCachePath, key) != undefined) {
         return toolPath;
     } else {
