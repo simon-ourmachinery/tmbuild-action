7c60c7d63738fa5244f4e7b5409feabc351cc67e
Simon Renger <simon@ourmachinery.com>
2022-05-25 14:34:00 +0200

    takeown correction

--- index.js
+++ index.js
@@ -442,7 +442,7 @@ async function build_engine(clang, build_config, project, package) {
         options.cwd = path;
 
     if (os.platform() == "win32") {
-        await exec.exec(`TAKEOWN /r /d Y /F ./bin`, [], options);
+        await exec.exec(`TAKEOWN /r /d Y /F bin`, [], options);
     }
 
     options.silent = !core.isDebug();
