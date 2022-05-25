2c14fc5d734b8f0ed8776717c6b2cea90a190d83
Simon Renger <simon@ourmachinery.com>
2022-05-25 14:38:32 +0200

    rm

--- index.js
+++ index.js
@@ -442,7 +442,6 @@ async function build_engine(clang, build_config, project, package) {
         options.cwd = path;
 
     if (os.platform() == "win32") {
-        await exec.exec(`ls`, [], options);
         await exec.exec(`TAKEOWN /r /d Y /F bin`, [], options);
     }
 
